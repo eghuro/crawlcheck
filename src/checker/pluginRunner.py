5cce2b7f1b3a4dc4eedf5ad929a5df3e11c6ae8b
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-15 00:35:12 +0200

    Plugin runner fix

--- src/checker/pluginRunner.py
+++ src/checker/pluginRunner.py
@@ -4,15 +4,16 @@ class PluginRunner:
     def __init__(self, dbconf):
         self.dbconf = dbconf
 
-    def runOne(self, plugin, api):
-        plugin.setDb(api)
-        info = api.getTransaction()
-        while info.getId() != -1:
+    def runTransaction(self, plugins, info):
+        for plugin in plugins:
             if plugin.handleContent(info.getContentType):
                 plugin.check(info.getId(), info.getContent())
-            info = api.getTransaction()
 
     def run(self, plugins):
         api = DBAPI(self.dbconf)
         for plugin in plugins:
-            self.runOne(plugin, api)
+            plugin.setDb(api)
+        info = api.getTransaction()
+        while info.getId() != -1:
+            runTransaction(self, plugins, info)
+            info = api.getTransaction()
