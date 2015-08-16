5bea86db0738ceae7be1d9c49a9ca81dadcee52c
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-16 05:11:37 +0200

    MySQL design modified, added constraints

--- src/checker/mysql_tables.sql
+++ src/checker/mysql_tables.sql
@@ -1,5 +1,12 @@
 USE crawlcheck;
 
+CREATE TABLE IF NOT EXISTS verificationStatus (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  status VARCHAR(255) NOT NULL,
+  description TEXT,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
 CREATE TABLE IF NOT EXISTS transaction (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
   method ENUM( 'GET', 'POST', 'PUT', 'CONNECT', 'HEAD', 'DELETE', 'TRACE') NOT NULL,
@@ -9,44 +16,57 @@ CREATE TABLE IF NOT EXISTS transaction (
   content TEXT,
   verificationStatusId INT UNSIGNED,
   origin ENUM( 'CLIENT', 'CHECKER'),
-  PRIMARY KEY(id)
-);
+  rawRequest BLOB,
+  rawResponse BLOB,
+  PRIMARY KEY(id),
+  FOREIGN KEY (verificationStatusId) 
+    REFERENCES verificationStatus(id)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS finding (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   responseId INT UNSIGNED NOT NULL,
-  PRIMARY KEY(id)
-);
+  PRIMARY KEY(id),
+  FOREIGN KEY(responseId)
+    REFERENCES transaction(id)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS link (
   findingId INT UNSIGNED NOT NULL,
   toUri VARCHAR(255) NOT NULL,
   processed BOOLEAN NOT NULL DEFAULT false,
   requestId INT UNSIGNED,
-  PRIMARY KEY(findingId)
-);
-
-CREATE TABLE IF NOT EXISTS defect (
-  findingId INT UNSIGNED NOT NULL,
-  type INT UNSIGNED NOT NULL,
-  location INT UNSIGNED NOT NULL,
-  evidence TEXT NOT NULL,
-  PRIMARY KEY(findingId)
-);
+  PRIMARY KEY(findingId),
+  FOREIGN KEY (findingId)
+    REFERENCES finding(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (requestId)
+    REFERENCES transaction(id)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS defectType (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   type VARCHAR(255) NOT NULL,
   description TEXT,
   PRIMARY KEY (id)
-);
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS verificationStatus (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  status VARCHAR(255) NOT NULL,
-  description TEXT,
-  PRIMARY KEY (id)
-);
+CREATE TABLE IF NOT EXISTS defect (
+  findingId INT UNSIGNED NOT NULL,
+  type INT UNSIGNED NOT NULL,
+  location INT UNSIGNED NOT NULL,
+  evidence TEXT NOT NULL,
+  PRIMARY KEY(findingId),
+  FOREIGN KEY (findingId)
+    REFERENCES finding(id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (type)
+    REFERENCES defectType(id)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS annotation (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -54,8 +74,11 @@ CREATE TABLE IF NOT EXISTS annotation (
   comment TEXT NOT NULL,
   author VARCHAR(255) NOT NULL,
   created DATETIME NOT NULL,
-  PRIMARY KEY (id)
-);
+  PRIMARY KEY (id),
+  FOREIGN KEY (findingId)
+    REFERENCES finding(id)
+    ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 INSERT INTO verificationStatus(id, status) VALUES (1, "REQUESTED");
 INSERT INTO verificationStatus(id, status) VALUES (2, "RETRIEVING");
