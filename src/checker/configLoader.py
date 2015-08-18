d6afd6ec58d29ca78058f134ff18042376f158bb
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-18 03:09:03 +0200

    ConfigLoader

--- src/checker/configLoader.py
+++ src/checker/configLoader.py
@@ -19,6 +19,15 @@ class ConfigLoader:
             self.dbconf.setPassword(dbAtts['pass'])
             self.dbconf.setDbname(dbAtts['dbname'])
             self.dbconf.setUser(dbAtts['user'])
+
+            plugins = root.find('plugins')
+
+            getResolutions(plugins, self.uriAcceptor, self.typeAcceptor)
+
+            pluginSet = plugins.find('plugin')
+            for plugin in pluginSet:
+                pluginId = plugin.attrib['id']
+                getPluginResolutions(pluginId, plugin, self.uriAcceptor, self.typeAcceptor)
         else:
             print "Configuration version doesn't match"
 
@@ -30,3 +39,29 @@ class ConfigLoader:
 
     def getUriAcceptor(self):
         return self.uriAcceptor
+
+    def getDefaults(self, root, setKeyword, keyword, acceptor):
+        defaultSet = root.find(setKeyword)
+        acceptor = Acceptor(defaultSet.attrib['default'] == 'True')
+
+        defaultElements = defaultSet.findAll(keyword)
+        for element in defaultElements:
+            acceptor.setDefaultAcceptValue(element.attrib['key'], element.attrib['accept'])
+
+    def getResolutions(self, root, uriAcceptor, typeAcceptor):
+        defaults = root.find('resolutions') 
+        getDefaults(defaults, 'uris', 'uri', uriAcceptor)
+        getDefaults(defaults, 'contentTypes', 'contentType', typeAcceptor)
+
+    def getPluginDefaults(self, pluginId, root, setKeyword, keyword, acceptor):
+        defaultSet = root.find(setKeyword)
+        acceptor.setPluginAcceptValueDefault(pluginId, defaultSet.attrib['default'] == 'True')
+
+        defaultElements = defaultSet.findAll(keyword)
+        for element in defaultElements:
+            acceptor.setPluginAcceptValue(pluginId, element.attrib['key'], element.attrib['accept'])
+
+    def getPluginResolutions(self, pluginId, root, uriAcceptor, typeAcceptor):
+        defaults = root.find('resolutions')
+        getPluginDefaults(pluginId, defaults, 'uris', 'uri', uriAcceptor)
+        getPluginDefaults(pluginId, defaults, 'contentTypes', 'contentType', typeAcceptor)
