8ea3a490e5f434ed9f3ceada643130bde4c9da6a
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-22 20:28:34 +0200

    Report fixes

--- src/report/app/controllers/finding_controller.rb
+++ src/report/app/controllers/finding_controller.rb
@@ -5,10 +5,12 @@ class FindingController < ApplicationController
     begin
       @transaction = Transaction.find(params[:id])
       @finding = Finding.find_by responseId: params[:id]
-      @findingId = @finding.id
-      Rails.logger.debug("#{@finding.id}")
-      @defects = Defect.where(findingId = @findingId).all
-      @links = Link.where(findingId = @findingId).all
+      if @finding
+        @findingId = @finding.id
+        Rails.logger.debug("#{@finding.id}")
+        @defects = Defect.where(findingId = @findingId).all
+        @links = Link.where(findingId = @findingId).all
+      end
     rescue ActiveRecord::RecordNotFound
     end
   end
