6b71f5f097feae6d630044e5bd7c98c31c946e99
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-24 09:18:14 +0200

    Proxy configuration object

--- src/proxy/proxy.rb
+++ src/proxy/proxy.rb
@@ -2,15 +2,75 @@ require 'webrick'
 require 'webrick/httpproxy'
 require 'mysql'
 
+class ProxyConfiguration
+  def initialize
+    @server = ''
+    @user = ''
+    @pass = ''
+    @dbname = ''
+    @port = -1
+  end
+
+  def setServer(name)
+    @server = name
+  end
+
+  def getServer
+    return @server
+  end
+
+  def setUser(user)
+    @user = user
+  end
+
+  def getUser
+    return @user
+  end
+
+  def setPassword(pass)
+    @pass = pass
+  end
+
+  def getPassword
+    return @pass
+  end
+
+  def setDb(name)
+    @dbname = name
+  end
+
+  def getDb
+    return @dbname
+  end
+
+  def setPort(port)
+    @port = port
+  end
+
+  def getPort
+    return @port
+  end
+
+  def self.create(server, user, pass, dbname, port)
+    conf = ProxyConfiguration.new
+    conf.setServer(server)
+    conf.setUser(user)
+    conf.setPassword(pass)
+    conf.setDb(dbname)
+    conf.setPort(port)
+    return conf
+  end
+end
+
 class Transaction
-  def initialize(method, uri, status, ctype, content)
+  def initialize(method, uri, status, ctype, content, configuration)
     @method = method
     @uri = uri
     @status = status
     @ctype = ctype
     @content = content
 
-    @con = Mysql.new 'localhost', 'test', '', 'crawlcheck'
+    @con = Mysql.new(configuration.getDb(), configuration.getUser(), configuration.getPassword(), configuration.getDb())
   end
 
   def getMySQLquery
@@ -30,11 +90,13 @@ class Transaction
 end
 
 def handle_content (req, res)
-  trans = Transaction.new(req.request_method(), req.unparsed_uri(), res.status, res.content_type(), res.body)
+  trans = Transaction.new(req.request_method(), req.unparsed_uri(), res.status, res.content_type(), res.body, conf)
   trans.pushDB
 end
 
-s = WEBrick::HTTPProxyServer.new(:Port => 8080, :ProxyContentHandler => method(:handle_content))
+
+conf = ProxyConfiguration.create('localhost', 'test', '', 'crawlcheck', 8080)
+s = WEBrick::HTTPProxyServer.new(:Port => conf.getPort(), :ProxyContentHandler => method(:handle_content))
 
 # Shutdown functionality
 trap("INT"){s.shutdown}
