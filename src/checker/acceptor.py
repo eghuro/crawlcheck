28551ea9139550ec806916b1f0a95bf29d184567
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-26 23:54:22 +0200

    Acceptor comments

--- src/checker/acceptor.py
+++ src/checker/acceptor.py
@@ -3,6 +3,16 @@
 
 Acceptor module contains budiness rules to decide if a transaction will be
 checked by a plugin.
+
+For a plugin it's possible to set which URIs should be checked and which should
+not as well as which content types should be checked and which should not.
+In case for an URI or a content type, it's not specified wheather it should be
+checked or not, there is a possibility to set up a default.
+In case for a plugin there's no configuration at all or it's missing for an URI
+or a content type and default for the plugin is not set, there's a global
+configuration. Global configuration is similar to a plugin-local configuration,
+there are URI and content type specific rules and a default fallback. The only
+exception is that the default fallback must be set.
 """
 from enum import Enum
 
@@ -31,6 +41,17 @@ class Acceptor(object):
         self.uris = set()
 
     def accept(self, pluginId, uri):
+        """ Does a plugin accept an URI?
+        Main API method, that runs a resolution algorithm.
+
+        Args:
+            pluginId: pluign identifier
+            URI: transaction's URI
+
+        Returns:
+           True if a plugin should check a transaction with URI given.
+           False otherwise
+        """
         res = self.pluginAcceptValue(pluginId, uri)
         if res == Resolution.yes:
             return True
