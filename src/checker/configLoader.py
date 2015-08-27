876e56327ce15f1a1da3a03be28386c865c30199
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-27 02:13:49 +0200

    ConfigLoader style and comments

--- src/checker/configLoader.py
+++ src/checker/configLoader.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Configuration Loader loads configuration from external XML file.
+"""
 import xml.etree.ElementTree as etree
-from pluginDBAPI import DBAPIconfiguration
-from acceptor import Acceptor
+from checker.pluginDBAPI import DBAPIconfiguration
+from checker.acceptor import Acceptor
 
-class ConfigLoader:
+
+class ConfigLoader(object):
+    """ ConfigLoader loads configuration from file.
+        Configuration is loaded through load()
+        Later, DB configuration, URI and Content-Type acceptors can be retrieved through getters.
+    """
     def __init__(self):
         self.version = "0.00"
         self.dbconf = DBAPIconfiguration()
@@ -10,6 +18,8 @@ class ConfigLoader:
         self.uriAcceptor = None
 
     def load(self, fname):
+        """Loads configuration from XML file.
+        """
         tree = etree.parse(fname)
         root = tree.getroot()
 
@@ -28,51 +38,67 @@ class ConfigLoader:
             pluginSet = plugins.findall('plugin')
             for plugin in pluginSet:
                 pluginId = plugin.attrib['id']
-                self.getPluginResolutions(pluginId, plugin, self.uriAcceptor, self.typeAcceptor)
+                self.getPluginResolutions(pluginId, plugin, self.uriAcceptor,
+                                          self.typeAcceptor)
         else:
             print "Configuration version doesn't match"
 
     def getDbconf(self):
+        """ Retrieve DB configuration.
+        """
         return self.dbconf
 
     def getTypeAcceptor(self):
+        """ Retrieve Acceptor instance for Content-Type.
+        """
         return self.typeAcceptor
 
     def getUriAcceptor(self):
+        """ Retrieve Acceptor instance for URI.
+        """
         return self.uriAcceptor
 
     def getDefaults(self, root, setKeyword, keyword):
         defaultSet = root.find(setKeyword)
         if defaultSet is not None:
-          acceptor = Acceptor(defaultSet.attrib['default'] == 'True')
+            acceptor = Acceptor(defaultSet.attrib['default'] == 'True')
 
-          defaultElements = defaultSet.findall(keyword)
-          if defaultElements is not None:
-            for element in defaultElements:
-              acceptor.setDefaultAcceptValue(element.attrib['key'], element.attrib['accept']=='True')
+            defaultElements = defaultSet.findall(keyword)
+            if defaultElements is not None:
+                for element in defaultElements:
+                    acceptor.setDefaultAcceptValue(element.attrib['key'],
+                                                   element.attrib['accept'] ==
+                                                   'True')
 
-          return acceptor
-        else:
-          return None
+                return acceptor
+            else:
+                return None
 
     def getResolutions(self, root):
-        defaults = root.find('resolutions') 
+        defaults = root.find('resolutions')
         if defaults is not None:
-          self.uriAcceptor = self.getDefaults(defaults, 'uris', 'uri')
-          self.typeAcceptor = self.getDefaults(defaults, 'contentTypes', 'contentType')
+            self.uriAcceptor = self.getDefaults(defaults, 'uris', 'uri')
+            self.typeAcceptor = self.getDefaults(defaults, 'contentTypes',
+                                                 'contentType')
 
     def getPluginDefaults(self, pluginId, root, setKeyword, keyword, acceptor):
         defaultSet = root.find(setKeyword)
         if defaultSet is not None:
-          acceptor.setPluginAcceptValueDefault(pluginId, defaultSet.attrib['default'] == 'True')
+            acceptor.setPluginAcceptValueDefault(pluginId,
+                                                 defaultSet.attrib['default'] ==
+                                                 'True')
 
-          defaultElements = defaultSet.findall(keyword)
-          if defaultElements is not None:
-            for element in defaultElements:
-              acceptor.setPluginAcceptValue(pluginId, element.attrib['key'], element.attrib['accept'] == 'True')
+            defaultElements = defaultSet.findall(keyword)
+            if defaultElements is not None:
+                for element in defaultElements:
+                    acceptor.setPluginAcceptValue(pluginId,
+                                                  element.attrib['key'],
+                                                  element.attrib['accept'] ==
+                                                  'True')
 
     def getPluginResolutions(self, pluginId, root, uriAcceptor, typeAcceptor):
         defaults = root.find('resolutions')
         if defaults is not None:
-          self.getPluginDefaults(pluginId, defaults, 'uris', 'uri', uriAcceptor)
-          self.getPluginDefaults(pluginId, defaults, 'contentTypes', 'contentType', typeAcceptor)
+            self.getPluginDefaults(pluginId, defaults, 'uris', 'uri', uriAcceptor)
+            self.getPluginDefaults(pluginId, defaults, 'contentTypes',
+                                   'contentType', typeAcceptor)
