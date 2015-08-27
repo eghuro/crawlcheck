08f191d514e7add3805e3c135eee6150b0c34a4e
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-27 06:04:44 +0200

    PluginRunner's docs and style

--- src/checker/pluginRunner.py
+++ src/checker/pluginRunner.py
@@ -1,8 +1,16 @@
-from pluginDBAPI import DBAPI
-from acceptor import Acceptor
+""" PluginRunner runs all transactions through applicable checkers.
+"""
+from checker.pluginDBAPI import DBAPI
 import marisa_trie
 
-class PluginRunner:
+
+class PluginRunner(object):
+    """ PluginRunner runs all transactions through applicable checkers.
+        A transaction is checked by a plugin if it is accepted by both
+        uri acceptor and content-type acceptor.
+        For acceptors, uri is changed to the longest prefix based on
+        plugin's configuration using trie.
+    """
     def __init__(self, dbconf, uriAcceptor, typeAcceptor):
         self.dbconf = dbconf
         self.pluginsById = {}
@@ -10,28 +18,32 @@ class PluginRunner:
         self.typeAcceptor = typeAcceptor
 
     def runTransaction(self, plugins, info):
+        """ Run a single transaction through all plugins where it's accepted.
+        """
         for plugin in plugins:
             if self.accept(plugin.getId(), info):
                 plugin.check(info.getId(), info.getContent())
 
     def run(self, plugins):
+        """ Run all transactions through all plugins where it's accepted.
+        """
         api = DBAPI(self.dbconf)
         for plugin in plugins:
             plugin.setDb(api)
             self.pluginsById[plugin.getId()] = plugin
         info = api.getTransaction()
         while info.getId() != -1:
-            info.setUri(self.getMaxPrefix(info.getUri())) # nahradi uri nejdelsim prefixem dle konfigurace pluginu
+            info.setUri(self.getMaxPrefix(info.getUri()))
+            # uri se nahradi nejdelsim prefixem dle konfigurace pluginu
             self.runTransaction(plugins, info)
             api.setFinished(info.getId())
             info = api.getTransaction()
 
     def accept(self, pluginId, transaction):
-        return self.uriAcceptor.accept(pluginId, transaction.getUri()) & self.typeAcceptor.accept(pluginId, transaction.getContentType())
+        return self.uriAcceptor.accept(pluginId, transaction.getUri()) and self.typeAcceptor.accept(pluginId, transaction.getContentType())
 
     def getMaxPrefix(self, uri):
-        prefixes = self.uriAcceptor.getValues() # seznam prefixu, pro nas uri chceme nejdelsi prefix
+        prefixes = self.uriAcceptor.getValues()
+        # seznam prefixu, pro nas uri chceme nejdelsi prefix
         trie = marisa_trie.Trie(prefixes)
         return trie.prefixes(uri)[-1]
-
-#        return marisa_trie.Trie(self.uriAcceptor.getValues()).prefixes(uri)[-1]
