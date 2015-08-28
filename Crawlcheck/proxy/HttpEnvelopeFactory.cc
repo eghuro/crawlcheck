6155b108a10c9ebe1b44a0da77f4c91926c3c8eb
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-29 01:55:20 +0200

    Cleanup

--- Crawlcheck/proxy/HttpEnvelopeFactory.cc
+++
@@ -1,8 +0,0 @@
-/*
- * HttpEnvelopeFactory.cc
- * Copyright 2015 Alexandr Mansurov
- */
-#include "../interface/header.h"
-
-crawlcheck::net::HttpEnvelopeFactory *
-  crawlcheck::net::HttpEnvelopeFactory::pInstance_ = 0;
