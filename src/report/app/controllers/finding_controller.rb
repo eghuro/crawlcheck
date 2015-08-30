67e0c17a701589bd334759a35f2033138552cb66
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-30 15:12:41 +0200

    Clustering

--- src/report/app/controllers/finding_controller.rb
+++ src/report/app/controllers/finding_controller.rb
@@ -3,8 +3,8 @@ class FindingController < ApplicationController
   helper_method :getDefectDescription
 
   def index
-    @defects = Defect.find_by_sql("select transaction.uri, location, evidence, defectType.description from defect inner join finding on finding.id = defect.findingId inner join defectType on defect.type = defectType.id inner join transaction on transaction.id = finding.responseId order by uri")
-    @links = Link.find_by_sql("select uri, toUri, processed from link join finding on finding.id = link.findingId join transaction on finding.responseId = transaction.id order by uri")
+    @defects = Defect.find_by_sql("select transaction.uri, location, evidence, defectType.description from defect inner join finding on finding.id = defect.findingId inner join defectType on defect.type = defectType.id inner join transaction on transaction.id = finding.responseId order by defectType.type")
+    @links = Link.find_by_sql("select distinct uri, toUri, processed from link inner join finding on finding.id = link.findingId inner join transaction on finding.responseId = transaction.id order by toUri")
   end
 
   def show
