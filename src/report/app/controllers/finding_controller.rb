3a191caf060e718d6031fe523d1080b7ab9a45c9
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-08-30 14:45:25 +0200

    List all findings

--- src/report/app/controllers/finding_controller.rb
+++ src/report/app/controllers/finding_controller.rb
@@ -2,18 +2,16 @@ require 'active_record/errors'
 class FindingController < ApplicationController
   helper_method :getDefectDescription
 
+  def index
+    @defects = Defect.find_by_sql("select transaction.uri, location, evidence, defectType.description from defect inner join finding on finding.id = defect.findingId inner join defectType on defect.type = defectType.id inner join transaction on transaction.id = finding.responseId order by uri")
+    @links = Link.find_by_sql("select uri, toUri, processed from link join finding on finding.id = link.findingId join transaction on finding.responseId = transaction.id order by uri")
+  end
+
   def show
-    @empty = true
     begin
       @transaction = Transaction.find(params[:id])
-      @finding = Finding.find_by responseId: params[:id]
-      if @finding
-        @findingId = @finding.id
-        Rails.logger.debug("#{@finding.id}")
-        @defects = Defect.find_by_sql("select * from defect where findingId in (select id from finding where responseId = #{params[:id]})")
-        @links = Link.find_by_sql("select * from link where findingId in (select id from finding where responseId = #{params[:id]})")
-
-      end
+      @defects = Defect.find_by_sql("select * from defect where findingId in (select id from finding where responseId = #{params[:id]})")
+      @links = Link.find_by_sql("select * from link where findingId in (select id from finding where responseId = #{params[:id]})")
     rescue ActiveRecord::RecordNotFound
     end
   end
