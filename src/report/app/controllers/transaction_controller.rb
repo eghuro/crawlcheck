f69fc872aca0a012a93db6bc664f75a53d8402e3
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-09-11 17:45:56 +0200

    Pagination on transactions list

--- src/report/app/controllers/transaction_controller.rb
+++ src/report/app/controllers/transaction_controller.rb
@@ -1,9 +1,10 @@
 require 'uri'
+require 'will_paginate'
 
 class TransactionController < ApplicationController
   helper_method :format
   def index
-    @transactions = Transaction.all.order('uri ASC')
+    @transactions = Transaction.all.order('uri ASC').paginate(page: params[:page], per_page: 25)
     @status = Status.all
   end
 
