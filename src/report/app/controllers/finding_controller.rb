d5cefe00677f6fe78cef6856d40c1f976d57229b
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-09-11 22:32:52 +0200

    Filter defects by type

--- src/report/app/controllers/finding_controller.rb
+++ src/report/app/controllers/finding_controller.rb
@@ -5,9 +5,15 @@ require 'will_paginate'
 class FindingController < ApplicationController
   helper_method :getDefectDescription
   helper_method :format
+  helper_method :getTypes
 
   def index
-    defect_query = "select transactions.uri, location, evidence, defectType.description, finding.responseId as responseId from defect inner join finding on finding.id = defect.findingId inner join defectType on defect.type = defectType.id inner join transactions on transactions.id = finding.responseId order by defectType.type"
+    defect_query = "select transactions.uri, location, evidence, defectType.description, finding.responseId as responseId from defect inner join finding on finding.id = defect.findingId inner join defectType on defect.type = defectType.id inner join transactions on transactions.id = finding.responseId "
+    if params.has_key?("d")
+      @filtered = true
+      defect_query += "where defect.type = "+params[:d]+" "
+    end
+    defect_query += "order by defectType.type"
     link_query = "select distinct uri, toUri, processed from link inner join finding on finding.id = link.findingId inner join transactions on finding.responseId = transactions.id order by toUri"
     @defects = Defect.paginate_by_sql(defect_query, page: params[:defect], per_page: 25)
     @links = Link.paginate_by_sql(link_query, page: params[:link], per_page: 25)
@@ -29,5 +35,9 @@ class FindingController < ApplicationController
 
   def format(uri)
     return URI.unescape(uri)
+  end
+
+  def getTypes
+    return Dtype.select(:id, :description).distinct
   end
 end
