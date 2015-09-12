0a59eccd2319c615aa2242719677f0f9aeda15a9
Alexandr Mansurov <alexander.mansurov@gmail.com>
2015-09-12 03:19:30 +0200

    Report - filter transactions by status

--- src/report/app/controllers/transaction_controller.rb
+++ src/report/app/controllers/transaction_controller.rb
@@ -3,12 +3,22 @@ require 'will_paginate'
 
 class TransactionController < ApplicationController
   helper_method :format
+  helper_method :getStatuses
   def index
-    @transactions = Transaction.all.order('uri ASC').paginate(page: params[:page], per_page: 25)
+    if params.has_key?("s")
+      @filtered = true
+      @transactions = Transaction.where(verificationStatusId: params[:s]).order('uri ASC').paginate(page: params[:page], per_page: 25)
+    else
+      @transactions = Transaction.all.order('uri ASC').paginate(page: params[:page], per_page: 25)
+    end
     @status = Status.all
   end
 
   def format(uri)
     return URI.unescape(uri)
+  end
+
+  def getStatuses
+    return Status.find_by_sql("select distinct verificationStatus.id, status from verificationStatus inner join transactions on verificationStatus.id = transactions.verificationStatusId order by status ASC")
   end
 end
