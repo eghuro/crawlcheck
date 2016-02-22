d1a579f496a233c72bbd3df5a6e13c203fc5bc9e
Alexandr Mansurov <alexander.mansurov@gmail.com>
2016-02-22 01:40:11 +0100

    travis

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd src/checker
 rm test.sqlite
 sqlite3 test.sqlite < mysql_tables.sql
 coverage erase
@@ -8,4 +9,4 @@ coverage run -a downTest.py
 coverage run -a pluginDBAPItest.py
 coverage run -a pluginRunnerTest.py
 coverage run -a py_w3c_html_validator_pluginTest.py
-coverage report --skip-covered -m
+#coverage report --skip-covered -m
