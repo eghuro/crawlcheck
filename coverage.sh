3de1cd4420dbcdbf4920718d5b8166dd2356dbd6
Alexandr Mansurov <alexander.mansurov@gmail.com>
2016-02-22 02:26:50 +0100

    travis

--- coverage.sh
+++ coverage.sh
@@ -9,4 +9,4 @@ coverage run -a downTest.py
 coverage run -a pluginDBAPItest.py
 coverage run -a pluginRunnerTest.py
 coverage run -a py_w3c_html_validator_pluginTest.py
-#coverage report --skip-covered -m
+coverage report --skip-covered -m
