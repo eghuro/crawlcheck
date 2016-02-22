b66bde57198798fe7d6f5a905e25809ea25e39ea
Alexandr Mansurov <alexander.mansurov@gmail.com>
2016-02-22 01:04:25 +0100

    Basic information retrieval

--- src/checker/plugin/form_checker.py
+++ src/checker/plugin/form_checker.py
@@ -1,4 +1,6 @@
 from yapsy.IPlugin import IPlugin
+from bs4 import BeautifulSoup
+import urlparse
 
 class Form_Checker(IPlugin):
     def __init__(self):
@@ -8,7 +10,27 @@ class Form_Checker(IPlugin):
         self.database = DB
 
     def check(self, transactionId, content):
-        pass
+        soup = BeautifulSoup(content, 'html.parser')
+        forms = soup.find_all('form')
+        for form in forms:
+            self.check_form(form, transactionId)
+
+    def check_form(self, form, transactionId):
+        method = self.get_method(form)
+        action = self.get_action(form, transactionId)
+
+    def get_method(self, form):
+        if 'method' in form.attrs:
+            return form['method'].to_lower()
+        else:
+            return 'get'
+
+    def get_action(self, form, transactionId): 
+        base = self.database.getUri(transactionId)
+        if 'action' in form.attrs:
+            return urlparse.urljoin(base, form['action'])
+        else:
+            return base
 
     def getId(self):
         return "formChecker"
