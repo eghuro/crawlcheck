9086b000598e34e8990fa9e122098ecf02e43e65
Alexandr Mansurov <alex@eghuro.cz>
2016-07-26 03:17:15 +0200

    Install script

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-pip install marisa_trie yapsy py_w3c enum34 urllib3 requests tinycss beautifulsoup4 pyyaml
-gem install rails
+sudo pip install marisa_trie yapsy py_w3c enum34 urllib3 requests tinycss beautifulsoup4 pyyaml
+sudo gem install bundler
+sudo gem install rails
 
 cd src/report
 bin/bundle install
 
 cp ../checker/mysql_tables.sql db/structure.sql
-sqlite3 $1 < ../checker/mysql_tables.sql
+sqlite3 ../crawlcheck < ../checker/mysql_tables.sql 2>/dev/null
 bin/rake db:migrate
 bin/rake db:migrate RAILS_ENV=development
-sqlite3 $1 < ../checker/mysql_tables.sql
+sqlite3 ../crawlcheck < ../checker/mysql_tables.sql 2>/dev/null
