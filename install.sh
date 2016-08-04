8493ff5d36b6f5cb27143b6ac93bdefc9e3ae518
Alexandr Mansurov <alex@eghuro.cz>
2016-08-04 22:26:38 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-
-sudo pip install marisa_trie yapsy py_w3c enum34 urllib3 requests tinycss beautifulsoup4 pyyaml
-sudo gem install bundler
-sudo gem install rails
+virtualenv env
+source env/bin/activate
+pip install marisa_trie yapsy py_w3c enum34 urllib3 requests tinycss beautifulsoup4 pyyaml
+gem install bundler
+gem install rails
 
 cd src/report
+#PATCH CFG
+python -c "
+	cfile = open('config/database.yml');
+	cfile.close()
+"
+
 bin/bundle install
 
 cp ../checker/mysql_tables.sql db/structure.sql
-sqlite3 ../crawlcheck < ../checker/mysql_tables.sql 2>/dev/null
+sqlite3 $1 < ../checker/mysql_tables.sql 2>/dev/null
 bin/rake db:migrate
 bin/rake db:migrate RAILS_ENV=development
-sqlite3 ../crawlcheck < ../checker/mysql_tables.sql 2>/dev/null
+sqlite3 $1 < ../checker/mysql_tables.sql 2>/dev/null
