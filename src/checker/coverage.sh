19545dafacb8eb40d4ea2b1b66064e9e6d769d7c
Alexandr Mansurov <alex@eghuro.cz>
2016-08-14 06:12:50 +0200

    Remove legacy tests

--- src/checker/coverage.sh
+++ src/checker/coverage.sh
@@ -4,9 +4,9 @@ sqlite3 crawlcheck < mysql_tables.sql
 coverage erase
 coverage run acceptorTest.py
 coverage run -a configLoaderTest.py
-coverage run -a downTest.py
-coverage run -a pluginDBAPItest.py
-coverage run -a pluginRunnerTest.py
+#coverage run -a downTest.py
+#coverage run -a pluginDBAPItest.py
+#coverage run -a pluginRunnerTest.py
 coverage run -a py_w3c_html_validator_pluginTest.py
 coverage run -a plugin/test_css_scraper.py
 coverage run -a paramDetectionTest.py
