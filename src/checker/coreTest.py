26d05b722c2a7104e21c94c0bb738b386cb557af
Alexandr Mansurov <alex@eghuro.cz>
2016-08-19 18:12:36 +0200

    Core test plan

--- src/checker/coreTest.py
+++ src/checker/coreTest.py
@@ -2,19 +2,50 @@
 # -*- coding: UTF-8 -*-
 
 import unittest
+from unittest.mock import patch
+import core
+from net import Network
+from configLoader import Configuration
 
 class TransactionTest(unittest.TestCase):
 
-    def testLoad(self):
+    def testFactory(self):
+        core.transactionId = 0
+        t1 = core.createTransaction('foobar')
+        self.assertEqual(t1.idno, 0)
+        self.assertEqual(core.transactionId, 1)
+        t2 = core.createTransaction('foobar')
+        self.assertEqual(t2.idno, 1)
+        self.assertEqual(core.transactionId, 2)
+
+    @patch('net.Network.getLink')
+    def testLoad(self, mock_get_link):
+        mock_get_link.return_value = 'text/html', '/tmp/foobar'
+        conf = Configuration(None, None, None, None, None, None, None, None, None)
+        t = core.createTransaction('foobar')
+        t.loadResponse(conf)
+        self.assertEqual(t.type, 'text/html')
+        self.assertEqual(t.file, '/tmp/foobar')
+        mock_get_link.assert_called_with(t, [], conf)
+
+    def testLoadNotTouchable(self):
+        pass
+
+    def testLoadNetworkError(self):
         pass
 
     def testContent(self):
+        # create temp
+        # put content in
+        # create transaction
+        # set file
+        # get content and assert
         pass
 
-    def testTouch(self):
-        pass
-
-    def testPrefix(self):
+    def testAcceptedTypes(self):
+        #prepare conf
+        #call
+        #assert
         pass
 
 
