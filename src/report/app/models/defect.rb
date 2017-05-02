c8fe0a55022566fa9d1faa09c78d7cfd38053db2
Alexandr Mansurov <alex@eghuro.cz>
2017-05-02 17:02:49 +0200

    Remove RoR report app

--- src/report/app/models/defect.rb
+++
@@ -1,6 +0,0 @@
-class Defect < ActiveRecord::Base
-  private
-    def self.inheritance_column
-      nil
-    end
-end
--- src/report/app/models/status.rb
+++
@@ -1,3 +0,0 @@
-class Status < ActiveRecord::Base
-  self.table_name = "verificationStatus"
-end
