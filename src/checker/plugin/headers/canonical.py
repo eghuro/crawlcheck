39e95cfe3f1e07e76d6eb0b6a4c3dc534af66c6a
Alexandr Mansurov <alex@eghuro.cz>
2017-05-03 21:20:30 +0200

    Cannonical URL header detection

---
+++ src/checker/plugin/headers/canonical.py
@@ -0,0 +1,27 @@
+from filter import FilterException
+from common import PluginType
+from yapsy.IPlugin import IPlugin
+import logging
+
+
+class CanonicalFilter(IPlugin):
+
+    category = PluginType.HEADER
+    id = "canonical"
+
+    def __init__(self):
+        self.__log = logging.getLogger(__name__)
+        self.__conf = None
+
+    def setConf(self, conf):
+        self.__conf = conf
+
+    def setJournal(self, journal):
+        pass
+
+    def filter(self, transaction, r):
+        if 'Link' in list(r.headers.keys()):
+            parts = r.headers['Link'].split(';')
+            if parts[1].strip() == 'rel="canonical"':
+                canonical = parts[0][1:-1] #Link: <http://example.com/page.html>; rel="canonical"
+                transaction.changePrimaryUri(canonical)
