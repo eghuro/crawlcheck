813a261747c5178a6efe0be678a07e89af87cebe
Alexandr Mansurov <alex@eghuro.cz>
2017-05-14 18:30:43 +0200

    Test IMG tags

---
+++ src/checker/plugin/checkers/seo_images.py
@@ -0,0 +1,46 @@
+from common import PluginType
+from bs4 import BeautifulSoup
+from yapsy.IPlugin import IPlugin
+import logging
+
+
+class ImageTagValidator(IPlugin):
+    
+    category = PluginType.CHECKER
+    id = "seoimg"
+    
+    
+    def __init__(self):
+        self.__journal = None
+
+    def setJournal(self, journal):
+        self.__journal = journal
+
+    def check(self, transaction):
+        """Pusti validator, ulozi chyby.
+        """
+
+        if 'soup' in transaction.cache and transaction.cache['soup']:
+            soup = transaction.cache['soup']
+        else:
+            soup = BeautifulSoup(transaction.getContent(), 'lxml')
+            transaction.cache['soup'] = soup
+
+        for img in soup.find_all('img'):
+            if 'src' in img:
+                desc = img['src']
+            else:
+                desc = ""
+            if 'width' not in img:
+                self.__journal.foundDefect(transaction.idno,
+                                           "seo:img:nowidth",
+                                           "IMG tag with no width attribute",
+                                           desc, 0.6)
+            if 'height' not in img:
+                self.__journal.foundDefect(transaction.idno,
+                                           "seo:img:noheight",
+                                           "IMG tag with no height attribute",
+                                           desc, 0.6)
+            #TODO: only jpg, png, gifs are allowed
+            #TODO: check IMG dimensions match declared ones
+        return
