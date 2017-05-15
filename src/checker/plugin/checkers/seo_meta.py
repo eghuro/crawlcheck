1d79e4f8a5c735199d2c4c7678c9a805fb9d9a2c
Alexandr Mansurov <alex@eghuro.cz>
2017-05-15 12:54:17 +0200

    Refactoring

--- src/checker/plugin/checkers/seo_meta.py
+++ src/checker/plugin/checkers/seo_meta.py
@@ -9,12 +9,30 @@ class MetaTagValidator(IPlugin):
     category = PluginType.CHECKER
     id = "seometa"
     
-    
+    __defects = {"seo:multidsc": "Multiple description meta tags found",
+                 "seo:nodsc": "No description meta tag found",
+                 "seo:multikeys": "Multiple keywords meta tags found", 
+                 "seo:nokeys": "No keywords meta tags found"}
+    __severity = 0.8
+     
     def __init__(self):
         self.__journal = None
 
     def setJournal(self, journal):
         self.__journal = journal
+
+    def __classify(self, head, name, idno, zero, multi):
+        foo = head.find_all('meta', {'name' : name})
+        if len(foo) > 1:
+            clas = multi
+            dsc = str(len(desc)
+        elif len(foo) == 0:
+            clas = zero
+            dsc = ""
+
+        self.__journal.foundDefect(idno, clas,
+                                   MetaTagValidator.__defects[clas],
+                                   dsc, MetaTagValidator.__severity)
 
     def check(self, transaction):
         """Pusti validator, ulozi chyby.
@@ -27,27 +45,9 @@ class MetaTagValidator(IPlugin):
 
         for html in soup.find_all('html', limit=1):
             for head in html.find_all('head', limit=1):
-                desc = head.find_all('meta', {'name' : 'description'})
-                if len(desc) > 1:
-                    self.__journal.foundDefect(transaction.idno,
-                                               "seo:multidsc",
-                                               "Multiple description meta tags found",
-                                               str(len(desc)), 0.8)
-                elif len(desc) == 0:
-                    self.__journal.foundDefect(transaction.idno,
-                                               "seo:nodsc",
-                                               "No description meta tag found",
-                                               "", 0.8)
+                self.__classify(head, 'description', transaction.idno,
+                                  "seo:nodsc", "seo:multidsc")
 
-                tags = head.find_all('meta', {'name' : 'keywords'})
-                if len(tags) > 1:
-                    self.__journal.foundDefect(transaction.idno,
-                                               "seo:multikeys",
-                                               "Multiple keywords meta tags found",
-                                               str(len(tags)), 0.8)
-                elif len(desc) == 0:
-                    self.__journal.foundDefect(transaction.idno,
-                                               "seo:nokeys",
-                                               "No keywords meta tag found",
-                                               "", 0.8)
+                self.__classify(head, 'keywords', transaction.idno,
+                                  "seo:nokeys", "seo:multikeys")
         return
