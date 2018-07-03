1dfcbf9eb8ddd19219a39c2be710e3e4d6d22703
Alexandr Mansurov <alex@eghuro.cz>
2018-07-03 22:42:41 +0200

    Dupdet refactoring

--- src/checker/plugin/checkers/dupdet.py
+++ src/checker/plugin/checkers/dupdet.py
@@ -42,48 +42,17 @@ class DuplicateDetector(IPlugin):
             self.__red = redis.StrictRedis(host=transaction.conf.getProperty('redisHost', 'localhost'),
                                            port=transaction.conf.getProperty('redisPort', 6379),
                                            db=transaction.conf.getProperty('redisDb', 0))
-        fsize = transaction.cache['size']
-        self.__red.hset("dupurl", transaction.file, transaction.uri)
-        self.__duptest(fsize, transaction)
-        return
-
-    def __duptest(self, fsize, transaction):
-        """Stage 1 of the duplication testing.
-        If the file size was already seen, we look for possible duplicates.
-        Otherwise just record the size.
-        """
-
-        if self.__red.hexists("dupsize", fsize):
-            h = self.__hashfile(transaction)
-            self.__red.hset("duphash", transaction.file, h)
-            self.__compare(fsize, transaction, h)
-            lst = set(json.loads(self.__red.hget("dupsize", fsize)))
-            lst.add(transaction.file)
-            self.__red.hset("dupsize", fsize, json.dumps(list(lst)))
+        h = self.__hashfile(transaction)
+        if self.__red.pfadd("duphash", h) == 1:
+            # duplicate
+            self.__journal.foundDefect(transaction.idno,
+                                       "dup",
+                                       "Duplicate pages",
+                                       self.__red.hget("dupname", h),
+                                       0.7)
         else:
-            self.__red.hset("dupsize", fsize, json.dumps([transaction.file]))
-
-    def __compare(self, fsize, transaction, reference_hash):
-        """Stage 2 of the duplication testing.
-        Compare the transaction for possible duplication candidates.
-
-        Returns a list of files listed in the set, that were removed
-        (possibly due to tmp limit)
-        """
-
-        for f in json.loads(self.__red.hget("dupsize", fsize)):
-            if self.__are_different(f, transaction):
-                if not self.__red.hexists("duphash", f):
-                    self.__red.hset("duphash", f, self.__hashfile(transaction))
-                if self.__red.hget("duphash", f) == reference_hash:
-                    self.__journal.foundDefect(transaction.idno,
-                                               "dup",
-                                               "Duplicate pages",
-                                               self.__red.hget("dupurl", f),
-                                               0.7)
-
-    def __are_different(self, file, transaction):
-        return file != transaction.file and self.__red.hget("dupurl", file) != transaction.uri
+            self.__red.hset("dupname", h, transaction.uri)
+        return
 
     def __hashfile(self, tr, blocksize=65536):
         hasher = hashlib.sha512()
